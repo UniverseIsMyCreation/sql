fa1aee06010ea667a9c3d43642eb1acbf6df99b8
Universe_Is_My_Creation <95547487+UniverseIsMyCreation@users.noreply.github.com>
2022-12-25 13:15:10 +0300

    select_requests

--- select_requests.sql
+++ select_requests.sql
@@ -27,3 +27,40 @@ round(case author
     else price end, 2) as new_price
 from book
 
+select author,title,price from book where amount<10
+
+select title,author,price,amount from book
+where price*amount>=5000 and (price<=500 or price >= 600)
+
+select title,author from book
+where price between 540.50 and 800 and amount in (2,3,5,7)
+
+select author,title from book
+where amount between 2 and 14
+order by author desc,title asc
+
+select title,author from book 
+where author like '%С.%'
+and title like '_% _%'
+order by title asc
+
+select 'Донцова Дарья' as author,
+concat('Евлампия романова и ',title) as title,
+round(price*1.42,2) as price
+from book
+order by price desc, title desc
+
+select 
+    author,title, 
+    price as real_price,
+    amount,
+    round(if(price*amount>5000,price*1.2,price*0.8),2) as new_price,
+    case
+        when price<500 then 99.99
+        when amount<5 then 149.99
+        else 0.00
+    end as delivery_price
+from book 
+where author in ('Булгаков М.А.','Есенин С.А.') and
+amount between 3 and 14
+order by author asc,title desc,delivery_price asc
